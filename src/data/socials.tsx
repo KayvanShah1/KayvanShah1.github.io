8d10bbb31e201f75f761d9beff5621dc5e0d5f11
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-13 17:46:46 -0700

    feat: add additional social media links for enhanced connectivity

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -1,4 +1,5 @@
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaGoogleScholar, FaKaggle } from "react-icons/fa6";
 
 const socialLinks = [
 	{
@@ -15,6 +16,21 @@ const socialLinks = [
 		icon: <FaInstagram className="h-5 w-5" />,
 		href: "https://instagram.com/kayvanshah", // Replace with your link
 		label: "Instagram",
+	},
+	{
+		icon: <FaFacebook className="h-5 w-5" />,
+		href: "https://www.facebook.com/kayvan.shah.108", // Replace with your link
+		label: "Facebook",
+	},
+	{
+		icon: <FaGoogleScholar className="h-5 w-5" />,
+		href: "https://scholar.google.com/citations?user=zNzHN0MAAAAJ&hl=en", // Replace with your link
+		label: "Google Scholar",
+	},
+	{
+		icon: <FaKaggle className="h-5 w-5" />,
+		href: "https://www.kaggle.com/kayvanshah", // Replace with your link
+		label: "Kaggle",
 	},
 ];
 
