351a6771f443214c952062686e2a531aa70319b0
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-13 18:02:07 -0700

    fix: correct syntax by adding missing semicolon in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "path";
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -7,9 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	base: "",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
