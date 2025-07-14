49acdd39e77396e47213fab0d2fed1e509a56560
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-13 17:47:14 -0700

    refactor: remove unused main section and related comments from App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,7 @@ function App() {
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<div className="no-scrollbar min-h-screen flex-col overflow-x-hidden">
 				<Navbar />
-
 				<Hero />
-
-				<main className="container mt-10 flex-1 px-24 pt-20">
-					{/* This is where you place the main sections like About, Projects, etc. */}
-
-					<section id="home">Home Section</section>
-					<section id="about">About Section</section>
-					<section id="experience">Experience Section</section>
-					<section id="projects">Projects Section</section>
-				</main>
-
 				<Footer />
 			</div>
 		</ThemeProvider>
