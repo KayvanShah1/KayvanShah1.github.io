9027c09cec91c2c43ca5e3765b7ac9698c20643f
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-07-18 18:26:31 -0700

    react dom and browser route on unknown paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { ThemeProvider } from "@/components/theme-provider";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import { ThemeProvider } from "@/components/theme-provider";
+
+function HomePage() {
+	return (
+		<div className="no-scrollbar min-h-screen flex-col overflow-x-hidden">
+			<Navbar />
+			<Hero />
+			<Education />
+			<Contact />
+			<Footer />
+		</div>
+	);
+}
 
 function App() {
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<div className="no-scrollbar min-h-screen flex-col overflow-x-hidden">
-				<Navbar />
-				<Hero />
-				<Education />
-				<Contact />
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<Routes>
+					{/* main route */}
+					<Route path="/" element={<HomePage />} />
+
+					{/* catch-all redirect */}
+					<Route path="*" element={<Navigate to="/" replace />} />
+				</Routes>
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 }
