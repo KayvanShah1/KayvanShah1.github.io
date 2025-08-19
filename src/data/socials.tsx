7ffb7eda95afbf59eaf1809af0e7a529034196d6
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-08-18 17:07:51 -0700

    added svg icons

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -5,41 +5,37 @@ import { HiMail } from "react-icons/hi";
 const socialLinks = [
 	{
 		icon: <FaGithub className="h-5 w-5" />,
+		svg: FaGithub,
 		href: "https://github.com/KayvanShah1", // Replace with your link
 		label: "GitHub",
 	},
 	{
 		icon: <FaLinkedin className="h-5 w-5" />,
+		svg: FaLinkedin,
 		href: "https://linkedin.com/in/kayvanshah999", // Replace with your link
 		label: "LinkedIn",
 	},
 	{
 		icon: <FaMedium className="h-5 w-5" />,
+		svg: FaMedium,
 		href: "https://medium.com/@KayvanShah1", // Replace with your link
 		label: "Medium",
 	},
-	// {
-	// 	icon: <FaInstagram className="h-5 w-5" />,
-	// 	href: "https://instagram.com/kayvanshah", // Replace with your link
-	// 	label: "Instagram",
-	// },
-	// {
-	// 	icon: <FaFacebook className="h-5 w-5" />,
-	// 	href: "https://www.facebook.com/kayvan.shah.108", // Replace with your link
-	// 	label: "Facebook",
-	// },
 	{
 		icon: <FaGoogleScholar className="h-5 w-5" />,
+		svg: FaGoogleScholar,
 		href: "https://scholar.google.com/citations?user=zNzHN0MAAAAJ&hl=en", // Replace with your link
 		label: "Google Scholar",
 	},
 	{
 		icon: <FaKaggle className="h-5 w-5" />,
+		svg: FaKaggle,
 		href: "https://www.kaggle.com/kayvanshah", // Replace with your link
 		label: "Kaggle",
 	},
 	{
 		icon: <HiMail className="h-5 w-5" />,
+		svg: HiMail,
 		href: "mailto:kayvan.taiyo@gmail.com", // Replace with your link
 		label: "Email",
 	},
