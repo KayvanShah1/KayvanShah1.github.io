6442abcad0338120077fa82b157fcc97c8f0ef09
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-08-20 01:17:49 -0700

    final combined project component

---
+++ src/components/features/profile/Projects.tsx
@@ -0,0 +1,18 @@
+import { PROJECTS } from "@/data/projects";
+import { CollapsibleList } from "@/components/collapsible-list";
+import { ProjectItem } from "@/components/features/profile/projects/project-item";
+
+const Projects = () => {
+	return (
+		<section id="projects" className="mx-6 my-10 flex flex-col justify-center py-10 sm:px-8 md:mx-4 md:px-16">
+			<h1 className="mb-8 text-4xl font-semibold">
+				Projects
+				<sup className="text-muted-foreground ml-1 text-sm select-none">({PROJECTS.length})</sup>
+			</h1>
+
+			<CollapsibleList items={PROJECTS} max={4} renderItem={(item) => <ProjectItem project={item} />} />
+		</section>
+	);
+};
+
+export default Projects;
