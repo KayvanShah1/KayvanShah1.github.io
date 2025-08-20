9d2ee970064d3a512d6472a2090ed4cd09265dc5
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-08-20 01:16:17 -0700

    added tooltip component

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -43,4 +43,13 @@ function TooltipContent({
 	);
 }
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+function SimpleTooltip({ children, content }: { children: React.ReactNode; content: React.ReactNode }) {
+	return (
+		<Tooltip>
+			<TooltipTrigger asChild>{children}</TooltipTrigger>
+			<TooltipContent>{content}</TooltipContent>
+		</Tooltip>
+	);
+}
+
+export { SimpleTooltip, Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
