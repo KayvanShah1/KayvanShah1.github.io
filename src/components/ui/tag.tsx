0e07f55ee4151f0cd4b28b14c0cf30efd895046f
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-08-20 01:16:02 -0700

    added custom tag component

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -6,7 +6,8 @@ function Tag({ className, ...props }: React.ComponentProps<"span">) {
 	return (
 		<span
 			className={cn(
-				"text-muted-foreground inline-flex items-center rounded-lg border bg-zinc-50 px-1.5 py-0.5 font-mono text-xs dark:bg-zinc-900",
+				"text-muted-foreground inline-flex items-center rounded-lg border bg-zinc-50 px-1.5 py-0.5 font-mono",
+				"text-xs dark:bg-zinc-900",
 				className
 			)}
 			{...props}
