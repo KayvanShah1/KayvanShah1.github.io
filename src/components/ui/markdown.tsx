358d800e1cf8ba4c454142fd032d65a5e85d37d2
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-08-21 02:25:54 -0700

    add double percentage plugin support to md

--- src/components/ui/markdown.tsx
+++ src/components/ui/markdown.tsx
@@ -5,16 +5,22 @@ import remarkGfm from "remark-gfm";
 
 import { UTM_PARAMS } from "@/config/site";
 import { rehypeAddQueryParams } from "@/lib/rehype-add-query-params";
+import { remarkDoublePercentageMark } from "@/lib/remark-double-per-mark";
 
 // Works as a Server or Client component (no async component involved)
 export function Markdown(props: React.ComponentProps<typeof ReactMarkdown>) {
 	return (
 		<ReactMarkdown
-			remarkPlugins={[remarkGfm]}
+			remarkPlugins={[remarkGfm, remarkDoublePercentageMark]}
 			rehypePlugins={[
 				[rehypeExternalLinks, { target: "_blank", rel: "nofollow noopener noreferrer" }],
 				[rehypeAddQueryParams, UTM_PARAMS],
 			]}
+			components={{
+				// strong: ({ children }) => <span className="font-semibold text-green-400">{children}</span>,
+				em: ({ children }) => <span className="text-yellow-400 italic">{children}</span>,
+				mark: ({ children }) => <span className="bg-transparent font-bold text-green-400">{children}</span>,
+			}}
 			{...props}
 		/>
 	);
