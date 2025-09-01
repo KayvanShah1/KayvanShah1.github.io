f44ddf96b3863bd7122dfa66dd3f47dcded8d6a9
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-09-01 15:55:40 -0700

    feat: added shimmer button in place of regular button change env icon to wave hand

--- src/components/features/profile/Hero.tsx
+++ src/components/features/profile/Hero.tsx
@@ -1,15 +1,15 @@
-import { FaEnvelope } from "react-icons/fa";
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
 import P5Canvas from "@/components/P5Canvas";
 import circuitTreeSmoothGrowth from "@/components/sketches/circuitTreeSmoothGrowth";
 import Typewriter from "@/components/Typewriter";
-import { Button } from "@/components/ui/button";
-import { ScrollProgress } from "@/components/magicui/scroll-progress";
+import { MdWavingHand } from "react-icons/md";
 
 const Hero = () => {
 	return (
 		<section id="hero" className="relative mb-10 flex w-full flex-col items-center justify-center px-6 py-10">
 			<ScrollProgress className="top-16 z-50 w-full bg-gradient-to-r from-green-400 via-teal-400 to-green-400" />
-			<P5Canvas sketch={circuitTreeSmoothGrowth} className="mt-20 mb-10 items-center" />
+			<P5Canvas sketch={circuitTreeSmoothGrowth} className="mt-20 mb-10 w-fit items-center" />
 
 			<h1 className="text-center text-4xl font-semibold sm:text-5xl lg:text-6xl xl:text-7xl" id="hero-title">
 				<Typewriter speed={100} cursor>
@@ -32,18 +32,19 @@ const Hero = () => {
 				raw data, applied machine learning, and impactful products.
 			</div>
 
-			<Button
-				variant="outline"
-				size="lg"
-				className="bg-foreground mt-10 mb-4 gap-2 text-lg text-green-400"
-				asChild
-			>
-				{/* <FaEnvelope size={40} /> Say Hi! */}
-				<a href="#contact" className="flex items-center gap-2">
-					<FaEnvelope size={20} />
-					<span>Say Hi!</span>
-				</a>
-			</Button>
+			<a href="#contact">
+				<ShimmerButton
+					className="mt-10 inline-flex items-center gap-2 text-green-400 shadow-2xl"
+					shimmerColor="oklch(79.2% 0.209 151.711)"
+					shimmerSize="0.15em"
+					shimmerDuration="3s"
+				>
+					<MdWavingHand size={20} className="text-green-400" />
+					<span className="text-center text-sm leading-none font-medium tracking-tight whitespace-pre-wrap text-green-400 lg:text-lg">
+						Say Hi!
+					</span>
+				</ShimmerButton>
+			</a>
 		</section>
 	);
 };
